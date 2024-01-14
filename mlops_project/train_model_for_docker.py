d5612fb59a83d1f7b79eebbf866c495e1970b02d
marcink11 <s232429@student.dtu.dk>
2024-01-14 22:17:57 +0100

    Add dockerfile

---
+++ mlops_project/train_model_for_docker.py
@@ -0,0 +1,19 @@
+# Use this script inside docker to run sequentially:
+# - process data
+# - train
+# - visualize
+
+import subprocess
+
+def run_script(script_name):
+    try:
+        # Run the script
+        completed_process = subprocess.run(["python", script_name], check=True)
+        print(f"Successfully executed {script_name}")
+    except subprocess.CalledProcessError as e:
+        print(f"Error occurred while executing {script_name}: {e}")
+        
+if __name__ == "__main__":
+    run_script("mlops_project/data/make_dataset.py")
+    run_script("mlops_project/train_model.py")
+    run_script("mlops_project/visualizations/visualize.py")
